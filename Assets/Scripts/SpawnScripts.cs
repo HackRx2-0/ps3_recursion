b398ab5935475915da322382ac43907749cb8666
HemangMaan <hemangmaan@gmail.com>
2021-07-24 03:12:45 +0530

    Developing Balloon Pop game

---
+++ Assets/Scripts/SpawnScripts.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScripts : MonoBehaviour
+{
+    public Transform[] spawnPooints;
+    public GameObject[] ballons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
